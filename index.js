d3717417c6a891673baae342eead172c0c32801a
Jacky Nguyen <nktpro@gmail.com>
2015-10-31 16:34:16 -0400

    Move decorator before classes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
     require("babel-plugin-transform-es2015-function-name"),
     require("babel-plugin-transform-es2015-arrow-functions"),
     require("babel-plugin-transform-es2015-block-scoped-functions"),
+    require("babel-plugin-transform-decorators"),
     require("babel-plugin-transform-es2015-classes"),
     require("babel-plugin-transform-es2015-object-super"),
     require("babel-plugin-transform-es2015-shorthand-properties"),
@@ -24,7 +25,6 @@ module.exports = {
     require("babel-plugin-transform-react-jsx"),
     require("babel-plugin-transform-flow-strip-types"),
     require("babel-plugin-syntax-flow"),
-    require("babel-plugin-syntax-jsx"),
-    require("babel-plugin-transform-decorators"),
+    require("babel-plugin-syntax-jsx")
   ]
 };
